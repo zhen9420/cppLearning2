f34bd1746c539bddeec1976772bec6ab33537dc7
zhen9420 <128135758+zhen9420@users.noreply.github.com>
2023-03-17 18:44:01 +0800

    Update 3_15_6.cpp

--- 3_15_6.cpp
+++ 3_15_6.cpp
@@ -1,13 +1,6 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(void){
-	
-	double n;
-	cin>>n;
-	double g5 = pow(5.0,1.0/2);
-	double c1=pow((1+g5)/2,n);
-	double c2 = pow((1-g5)/2,n);
-	double ans = (c1-c2)/g5;
-	cout<<fixed<<setprecision(2)<<ans;
+#include <iostream>
 
+int main() {
+	std::cout << "Helloworld";
+	return 0;
 }
