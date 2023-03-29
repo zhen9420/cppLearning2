86bc9ccdfc27f7625525bba008390f64176a5a80
zhen9420 <128135758+zhen9420@users.noreply.github.com>
2023-03-29 12:42:28 +0800

    1
    1

---
+++ test_string.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	string s[100];
+	cin>>s[0];
+//	for(int i = 0;i<17;i++){
+//		if(s[0][i]<'0'||s[0][i]>'9'){
+//			cout<<0;
+//		}
+//		else cout<<1;
+//	}
+	cout<<s[0] [s[0].length()-1];
+}
