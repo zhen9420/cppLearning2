bf2be1473042f3d1afdf682064a1afd3b72128a5
zhen9420 <128135758+zhen9420@users.noreply.github.com>
2023-04-06 21:51:02 +0800

    1

---
+++ henupta/e.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(void) {
+	int t; cin >> t;
+	for (int i = 0; i < t; i++) {
+		int a[1001][1001] = {0};
+		int n, m, sx, sy, d, x;
+		cin >> n >> m >> sx >> sy >> d >> x;
+		for(int j =0;j<)
+
+	}
+}
